a5408f3424bd3a3877714ac40bb31ad275db9067
Dxnte16 <cse25-350@thuto.bac.ac.bw>
2026-05-19 11:57:49 +0200

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-var forms = document.querySelectorAll("form");
-
-forms.forEach(function(form) {
-    form.addEventListener("submit", function(event) {
-        event.preventDefault();
-        alert("Thank you - we will contact you.");
-        form.reset();
-    });
-});
